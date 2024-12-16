34ec3e9b35a9bc33eda038e6c0b0893c122627bf
LaraBerenguer <laraberenguer@outlook.com>
2024-12-16 18:00:18 +0100

    Set Router basename for React application

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import '../index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/react-budget-cards">
       <App />
     </Router>
   </React.StrictMode>
